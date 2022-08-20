15ee2df552745a34d75e1f954150b7016fb15f97
Yevhen Malyshko <memx86@gmail.com>
2022-08-20 15:55:11 +0300

    CreatePostsScreen markup and styles

--- screens/main/CreatePostsScreen.js
+++ screens/main/CreatePostsScreen.js
@@ -1,10 +1,132 @@
-import { View, Text } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  useWindowDimensions,
+} from "react-native";
+import { Feather } from "@expo/vector-icons";
 
 const CreatePostsScreen = () => {
+  const { width } = useWindowDimensions();
+
   return (
-    <View>
-      <Text>CreatePostsScreen</Text>
+    <View style={s.container}>
+      <TouchableOpacity activeOpacity={0.7}>
+        <View
+          style={{
+            ...s.imageWrapper,
+            width: width - 16 * 2,
+            height: (width - 16 * 2) / 1.43,
+          }}
+        >
+          <View style={s.cameraImageWrapper}>
+            <Image
+              source={require("../../assets/images/camera.png")}
+              style={s.cameraImage}
+            />
+          </View>
+        </View>
+        <Text style={s.imageText}>Загрузите фото</Text>
+      </TouchableOpacity>
+      <View style={s.inputWrapper}>
+        <TextInput
+          style={s.input}
+          placeholder="Название..."
+          placeholderTextColor="#BDBDBD"
+        />
+        <View style={s.locationInputWrapper}>
+          <TextInput
+            style={{ ...s.input, paddingLeft: 28 }}
+            placeholder="Местность..."
+            placeholderTextColor="#BDBDBD"
+          />
+          <Feather
+            name="map-pin"
+            size={24}
+            color="#BDBDBD"
+            style={s.locationIcon}
+          />
+        </View>
+      </View>
+      <TouchableOpacity style={s.btn} activeOpacity={0.7}>
+        <Text style={s.btnText}>Опубликовать</Text>
+      </TouchableOpacity>
     </View>
   );
 };
+
+const s = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 32,
+    paddingBottom: 32,
+    paddingLeft: 16,
+    paddingRight: 16,
+    backgroundColor: "#ffffff",
+  },
+  imageWrapper: {
+    backgroundColor: "#E8E8E8",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cameraImageWrapper: {
+    width: 60,
+    height: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 30,
+    backgroundColor: "#ffffff",
+  },
+  cameraImage: {
+    width: 24,
+    height: 24,
+  },
+  imageText: {
+    marginTop: 8,
+    fontFamily: "Roboto",
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#BDBDBD",
+  },
+  inputWrapper: {
+    marginTop: 32,
+    marginBottom: 32,
+  },
+  input: {
+    paddingTop: 16,
+    paddingBottom: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: "#E8E8E8",
+    fontFamily: "Roboto-Medium",
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#212121",
+  },
+  locationInputWrapper: {
+    position: "relative",
+    marginTop: 16,
+  },
+  locationIcon: {
+    position: "absolute",
+    top: 20,
+    left: 0,
+  },
+  btn: {
+    height: 51,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 100,
+    backgroundColor: "#FF6C00",
+  },
+  btnText: {
+    fontFamily: "Roboto",
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#ffffff",
+  },
+});
+
 export default CreatePostsScreen;
